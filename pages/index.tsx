30f3090a70031005e43717c8b024b5e37818e3f4
Tyler Deitz <tylerdeitz@gmail.com>
2020-02-16 18:20:53 -0600

    title, desc, favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -225,8 +225,17 @@ const data: ReadonlyArray<[
 const Home: NextPage = () => (
   <>
     <Head>
-      <title>Home</title>
       <link rel="icon" href="/favicon.ico" />
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
+      />
+      <meta
+        name="Description"
+        content="A mobile-friendly version of the DSA-LA 2020 voter guide. Great for bringing to the polls and using as a reference while voting."
+      />
+      <title>Retreat Left</title>
     </Head>
 
     <div className="grid">
