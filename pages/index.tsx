9c43cc1bb9d57ee5bd3bbcec1186cd3575ccc6a4
Tyler Deitz <tylerdeitz@gmail.com>
2020-07-15 18:34:12 -0700

    add filters

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,65 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Head from "next/head";
-import { NextPage } from "next";
+import { useRouter } from "next/router";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
 
 import { data, filterableRecommendationCategories } from "../data";
 
-const Home: NextPage = () => {
+type Filter = Record<string, string | string[] | undefined>;
+
+type SetFilter = (category: string, value: string | undefined) => void;
+
+type Props = {
+  initialFilter: Filter;
+};
+
+const useFilter = (
+  initialFilter: Filter
+): { filter: Filter; setFilter: SetFilter; resetFilter: () => void } => {
+  const router = useRouter();
+  const [filter, setFilterWithoutUrlEvent] = useState<Filter>(initialFilter);
+
+  const setFilter: SetFilter = (category, value) => {
+    const newFilter: Filter = { ...filter };
+
+    if (value) {
+      newFilter[category] = value;
+    } else {
+      delete newFilter[category];
+    }
+
+    router.replace({ pathname: router.pathname, query: newFilter });
+    setFilterWithoutUrlEvent(newFilter);
+  };
+
+  const resetFilter = () => {
+    router.replace({ pathname: router.pathname, query: {} });
+    setFilterWithoutUrlEvent({});
+  };
+
+  return { filter, setFilter, resetFilter };
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => ({
+  props: { initialFilter: context.query },
+});
+
+const Home: NextPage<InferGetServerSidePropsType<
+  typeof getServerSideProps
+>> = ({ initialFilter }) => {
+  const { filter, setFilter, resetFilter } = useFilter(initialFilter);
+
+  const hasFilters = !!Object.keys(initialFilter).length;
+
   const [selectedHeaderItem, setSelectedHeaderItem] = useState<
     null | "about" | "filter"
-  >(null);
-
-  const [filter, setFilter] = useState<Record<string, string | undefined>>({});
+  >(hasFilters ? "filter" : null);
 
   return (
     <>
@@ -119,67 +169,78 @@ const Home: NextPage = () => {
             </>
           )}
 
-          {selectedHeaderItem === "filter" &&
-            Array.from(data).map(([category, values]) => {
-              if (!filterableRecommendationCategories.has(category)) {
-                return null;
-              }
+          {selectedHeaderItem === "filter" && (
+            <>
+              {Array.from(data).map(([category, values]) => {
+                if (!filterableRecommendationCategories.has(category)) {
+                  return null;
+                }
 
-              const allValue = "All";
-              const filterValue = filter[category] || allValue;
+                const allValue = "All";
+                const filterValue = filter[category] || allValue;
 
-              return (
-                <div
-                  key={category}
-                  className="mt3 lh-copy relative f6 bg-white-15 br3 pv1 ph2"
-                >
-                  <select
-                    value={filterValue}
-                    className="absolute absolute--fill f5 w-100 h-100 o-0 input-reset"
-                    onChange={(ev) => {
-                      setFilter({
-                        ...filter,
-                        [category]:
+                return (
+                  <div
+                    key={category}
+                    className="mt3 lh-copy relative f6 bg-white-15 br3 pv1 ph2"
+                  >
+                    <select
+                      value={filterValue}
+                      className="absolute absolute--fill f5 w-100 h-100 o-0 input-reset"
+                      onChange={(ev) => {
+                        setFilter(
+                          category,
                           ev.target.value === allValue
                             ? undefined
-                            : ev.target.value,
-                      });
-                    }}
-                  >
-                    <option disabled={true}>{category}</option>
+                            : ev.target.value
+                        );
+                      }}
+                    >
+                      <option disabled={true}>{category}</option>
 
-                    <option value={allValue}>All</option>
+                      <option value={allValue}>All</option>
 
-                    {values.map((value) => {
-                      if (!value.subtitle) {
-                        return null;
-                      }
+                      {values.map((value) => {
+                        if (!value.subtitle) {
+                          return null;
+                        }
 
-                      return (
-                        <option
-                          key={category + value.subtitle}
-                          value={value.subtitle}
-                        >
-                          {value.subtitle}
-                        </option>
-                      );
-                    })}
-                  </select>
+                        return (
+                          <option
+                            key={category + value.subtitle}
+                            value={value.subtitle}
+                          >
+                            {value.subtitle}
+                          </option>
+                        );
+                      })}
+                    </select>
 
-                  <span className="white">{category}: </span>
-                  <span className="fw7 white">{filterValue}</span>
-                </div>
-              );
-            })}
+                    <span className="white">{category}: </span>
+                    <span className="fw6 white">{filterValue}</span>
+                  </div>
+                );
+              })}
+
+              {hasFilters && (
+                <button
+                  onClick={resetFilter}
+                  className="mt3 bg-cool-black white outline-none-focus input-reset b--white f7 ph2 pv1 br4 ba b--solid lh-title ttu touch-action-manipulation"
+                >
+                  Reset filter
+                </button>
+              )}
+            </>
+          )}
         </div>
 
-        {Array.from(data).map(([key, value], i) => {
+        {Array.from(data).map(([category, options], i) => {
           const recommendationCategoryId = encodeURIComponent(
-            key.replace(/\s+/g, "-").toLowerCase()
+            category.replace(/\s+/g, "-").toLowerCase()
           );
 
           return (
-            <Fragment key={key}>
+            <Fragment key={category}>
               <div className="bg-white flex">
                 {!!i && (
                   <div className="white mix-blend-mode-diff absolute left-dotted-line right-0 bt b--dash b--dashed bl-0 bb-0 br-0" />
@@ -188,66 +249,73 @@ const Home: NextPage = () => {
                   <a
                     href={`#${recommendationCategoryId}`}
                     id={recommendationCategoryId}
-                    className="cool-black no-underline sticky pv3 ph-gutter top-0 break-word lh-static f6 db outline-none-focus"
+                    className="scroll-bahavior-smooth cool-black no-underline sticky pv3 ph-gutter top-0 break-word lh-static f6 db outline-none-focus flex flex-column"
                   >
-                    {key}
+                    <span>{category}</span>
+                    {filter[category] && <span className="f7">(Filtered)</span>}
                   </a>
                 </div>
               </div>
 
               <div className="white pv3 ph-gutter flex flex-column lh-static measure">
-                {value.map(({ title, subtitle, desc, link }, i) => (
-                  <details
-                    key={title + subtitle}
-                    className={"details-reset" + (i ? " mt4" : "")}
-                  >
-                    <summary className="flex flex-column">
-                      <div className="flex flex-column items-start">
-                        {subtitle && (
-                          <span className="flex f6">{subtitle}</span>
-                        )}
-                        <span className="fw6">{title}</span>
-                        {title === "Bernie Sanders" && (
-                          <div className="mv1 w-100 aspect-ratio-bernie mw-bernie">
-                            <img
-                              alt=""
-                              className="absolute absolute--fill"
-                              src="/bernie.jpg"
-                            />
-                          </div>
-                        )}
-                        <span className="f7 ph2 mt1 br-pill ba b--solid lh-copy ttu open-bg-white open-cool-black open-b--white">
-                          Info
-                        </span>
-                      </div>
-                    </summary>
-
-                    {desc ? (
-                      desc.map((paragraph, j, { length }) => (
-                        <span key={key + j} className="db f6 tj mt1 ti4">
-                          {paragraph}
-                          {j === length - 1 && (
-                            <>
-                              {" ["}
-                              <a className="color-inherit fw6" href={link}>
-                                Source
-                              </a>
-                              {"]"}
-                            </>
+                {options
+                  .filter((option) =>
+                    filter[category]
+                      ? filter[category] === option.subtitle
+                      : true
+                  )
+                  .map(({ title, subtitle, desc, link }, i) => (
+                    <details
+                      key={title + subtitle}
+                      className={"details-reset" + (i ? " mt4" : "")}
+                    >
+                      <summary className="flex flex-column">
+                        <div className="flex flex-column items-start">
+                          {subtitle && (
+                            <span className="flex f6">{subtitle}</span>
                           )}
+                          <span className="fw6">{title}</span>
+                          {title === "Bernie Sanders" && (
+                            <div className="mv1 w-100 aspect-ratio-bernie mw-bernie">
+                              <img
+                                alt=""
+                                className="absolute absolute--fill"
+                                src="/bernie.jpg"
+                              />
+                            </div>
+                          )}
+                          <span className="f7 ph2 mt1 br-pill ba b--solid lh-title pv1 ttu open-bg-white open-cool-black open-b--white">
+                            Info
+                          </span>
+                        </div>
+                      </summary>
+
+                      {desc ? (
+                        desc.map((paragraph, j, { length }) => (
+                          <span key={category + j} className="db f6 tj mt1 ti4">
+                            {paragraph}
+                            {j === length - 1 && (
+                              <>
+                                {" ["}
+                                <a className="color-inherit fw6" href={link}>
+                                  Source
+                                </a>
+                                {"]"}
+                              </>
+                            )}
+                          </span>
+                        ))
+                      ) : (
+                        <span className="db f6 tj mt1">
+                          [
+                          <a className="color-inherit fw6" href={link}>
+                            Source
+                          </a>
+                          ]
                         </span>
-                      ))
-                    ) : (
-                      <span className="db f6 tj mt1">
-                        [
-                        <a className="color-inherit fw6" href={link}>
-                          Source
-                        </a>
-                        ]
-                      </span>
-                    )}
-                  </details>
-                ))}
+                      )}
+                    </details>
+                  ))}
               </div>
             </Fragment>
           );
